62ed8f5cfef800853a8cd4ddf48e8490b1248d4c
James Casella <james@casellaweb.com.au>
2026-08-08 12:18:58 +0800

    Rebalance history tile colors for snooze and dismiss.

--- lib/enums/log_action_type_code.dart
+++ lib/enums/log_action_type_code.dart
@@ -25,39 +25,39 @@ enum LogActionTypeCodeEnum {
 
 LogActionTypeCodeEnum? RA_logActionFromCode(int code) =>
     code < LogActionTypeCodeEnum.values.length
-        ? LogActionTypeCodeEnum.values[code]
-        : null;
+    ? LogActionTypeCodeEnum.values[code]
+    : null;
 
 extension LogActionTypeCodeDisplay on LogActionTypeCodeEnum {
   /// Label shown on History / Alarm Logs tiles and PDF export.
   String get displayName => switch (this) {
-        LogActionTypeCodeEnum.AutoSnooze => 'Auto Snooze',
-        LogActionTypeCodeEnum.ResetCounter => 'Reset Counter',
-        _ => name,
-      };
+    LogActionTypeCodeEnum.AutoSnooze => 'Auto Snooze',
+    LogActionTypeCodeEnum.ResetCounter => 'Reset Counter',
+    _ => name,
+  };
 
   Color get color => switch (this) {
-        LogActionTypeCodeEnum.Dismiss => RA_ColourStyles.secondary,
-        LogActionTypeCodeEnum.Snooze => RA_ColourStyles.sleepIndigo,
-        LogActionTypeCodeEnum.Skip => RA_ColourStyles.primary,
-        LogActionTypeCodeEnum.AutoSnooze => RA_ColourStyles.softCoral,
-        LogActionTypeCodeEnum.Create => RA_ColourStyles.mossGreen,
-        LogActionTypeCodeEnum.Edit => RA_ColourStyles.slateBlue,
-        LogActionTypeCodeEnum.Delete => RA_ColourStyles.dustyWine,
-        LogActionTypeCodeEnum.Duplicate => RA_ColourStyles.softPlum,
-        LogActionTypeCodeEnum.Pause => RA_ColourStyles.pauseOchre,
-        LogActionTypeCodeEnum.Resume => RA_ColourStyles.oliveMist,
-        LogActionTypeCodeEnum.Mute => RA_ColourStyles.mutedViolet,
-        LogActionTypeCodeEnum.Unmute => RA_ColourStyles.softSky,
-        LogActionTypeCodeEnum.ResetCounter => RA_ColourStyles.warmAmber,
-        LogActionTypeCodeEnum.Ring => RA_ColourStyles.ringEmber,
-        LogActionTypeCodeEnum.Recover => RA_ColourStyles.mossGreen,
-      };
+    LogActionTypeCodeEnum.Dismiss => RA_ColourStyles.softCoral,
+    LogActionTypeCodeEnum.Snooze => RA_ColourStyles.secondary,
+    LogActionTypeCodeEnum.Skip => RA_ColourStyles.primary,
+    LogActionTypeCodeEnum.AutoSnooze => RA_ColourStyles.sleepIndigo,
+    LogActionTypeCodeEnum.Create => RA_ColourStyles.mossGreen,
+    LogActionTypeCodeEnum.Edit => RA_ColourStyles.slateBlue,
+    LogActionTypeCodeEnum.Delete => RA_ColourStyles.dustyWine,
+    LogActionTypeCodeEnum.Duplicate => RA_ColourStyles.softPlum,
+    LogActionTypeCodeEnum.Pause => RA_ColourStyles.pauseOchre,
+    LogActionTypeCodeEnum.Resume => RA_ColourStyles.oliveMist,
+    LogActionTypeCodeEnum.Mute => RA_ColourStyles.mutedViolet,
+    LogActionTypeCodeEnum.Unmute => RA_ColourStyles.softSky,
+    LogActionTypeCodeEnum.ResetCounter => RA_ColourStyles.warmAmber,
+    LogActionTypeCodeEnum.Ring => RA_ColourStyles.ringEmber,
+    LogActionTypeCodeEnum.Recover => RA_ColourStyles.mossGreen,
+  };
 
   /// Soft bloom behind log tiles for the most salient action types.
   List<BoxShadow>? get tileGlow => switch (this) {
-        LogActionTypeCodeEnum.AutoSnooze => RA_ShapeStyles.softCoralGlow,
-        LogActionTypeCodeEnum.Dismiss => RA_ShapeStyles.tealGlow,
-        _ => null,
-      };
+    LogActionTypeCodeEnum.Snooze => RA_ShapeStyles.tealGlow,
+    LogActionTypeCodeEnum.Dismiss => RA_ShapeStyles.softCoralGlow,
+    _ => null,
+  };
 }
